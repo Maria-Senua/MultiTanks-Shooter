16227d5296d74b330c8dfeaaa5fc4f2b7e22b9da
Maria Smirnova <92306135+Maria-Senua@users.noreply.github.com>
2025-11-04 15:35:14 +0100

    add explosion effect to bullet

--- Assets/NGO_Minimal_Setup/Scripts/Projectile.cs
+++ Assets/NGO_Minimal_Setup/Scripts/Projectile.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Projectile : NetworkBehaviour
 {
     [SerializeField] float lifeTime = 5f;
+    [SerializeField] GameObject explosion;
 
     void Start()
     {
@@ -12,9 +13,16 @@ public class Projectile : NetworkBehaviour
             Invoke(nameof(Despawn), lifeTime);
     }
 
-    void OnCollisionEnter(Collision _)
+    void OnCollisionEnter(Collision collision)
     {
-        if (IsServer) Despawn();
+        //add explosion particles
+        var contactPoint = collision.contacts[0].point;
+        var boom = Instantiate(explosion, contactPoint, Quaternion.identity);
+        Debug.Log("BOOM " + explosion.activeInHierarchy);
+        Destroy(boom, 2f);
+        //if (IsServer) Despawn();
+        if (IsServer)
+            Invoke(nameof(Despawn), 0.05f);
     }
 
     void Despawn()
