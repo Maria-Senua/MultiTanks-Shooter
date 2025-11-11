e14b2a20a9b9f49ef2b2c9a40bf19bfd0b95a1a8
Maria Smirnova <92306135+Maria-Senua@users.noreply.github.com>
2025-11-11 16:19:12 +0100

    made bricks and teerra maybe networkable

--- Assets/DestructibleWallGenerator/DWG/Scripts/DWGColliderEnabler.cs
+++ Assets/DestructibleWallGenerator/DWG/Scripts/DWGColliderEnabler.cs
@@ -1,8 +1,18 @@
 ﻿using UnityEngine;
+using Unity.Netcode;
 using System.Collections;
 
-public class DWGColliderEnabler : MonoBehaviour {
-	void Start () {
+public class DWGColliderEnabler : NetworkBehaviour {
+	public override void OnNetworkSpawn() {
+		if (!IsServer) return;
+		EnableCollider();
+	}
+
+	
+
+	void EnableCollider()
+	{
+		Debug.Log("DWGColliderEnabler enabled");
 		if(gameObject.GetComponent<Collider>()){ // If this game object has a collider, continue
 			gameObject.GetComponent<Collider>().enabled = true; // Enable the collider
 			Destroy(GetComponent<DWGColliderEnabler>()); // Remove this script from the game object
--- Assets/NGO_Minimal_Setup/Scripts/TerraHoles.cs
+++ Assets/NGO_Minimal_Setup/Scripts/TerraHoles.cs
@@ -1,7 +1,8 @@
 using System;
 using UnityEngine;
+using Unity.Netcode;
 
-public class TerraHoles : MonoBehaviour
+public class TerraHoles : NetworkBehaviour
 {
     [SerializeField] private Terrain t;
 
@@ -9,59 +10,75 @@ public class TerraHoles : MonoBehaviour
     [SerializeField] private int holeHeight = 10;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private void Start()
+    public override void OnNetworkSpawn()
     {
         TerrainData terrainData = t.terrainData;
-        int res = terrainData.holesResolution;
-        bool[,] fullHoles = new bool[res, res];
+        //if (IsServer)
+        //{
+            int res = terrainData.holesResolution;
+            bool[,] fullHoles = new bool[res, res];
 
-        for (int x = 0; x < res; x++)
-        {
-            for (int z = 0; z < res; z++)
+            for (int x = 0; x < res; x++)
             {
-                fullHoles[x, z] = true;
+                for (int z = 0; z < res; z++)
+                {
+                    fullHoles[x, z] = true;
+                }
             }
-        }
 
-        terrainData.SetHoles(0, 0, fullHoles);
+            terrainData.SetHoles(0, 0, fullHoles);   
+        //}
+        
     }
 
     private void OnCollisionEnter(Collision other)
     {
+        if (!IsServer) return;
         if (other.gameObject.tag == "Bullet")
         {
             Vector3 contactPoint = other.contacts[0].point;
 
-            TerrainData terrainData = t.terrainData;
-            Vector3 terrainPos = t.transform.position;
-
-            float relativeX = (contactPoint.x - terrainPos.x) / terrainData.size.x;
-            float relativeZ = (contactPoint.z - terrainPos.z) / terrainData.size.z;
-
-            int holeMapX = (int)(relativeX * terrainData.holesResolution);
-            int holeMapZ = (int)(relativeZ * terrainData.holesResolution);
-
-            int offsetX = holeWidth / 2;
-            int offsetZ = holeHeight / 2;
-
-            int startX = Mathf.Clamp(holeMapX - offsetX, 0, terrainData.holesResolution - holeWidth);
-            int startZ = Mathf.Clamp(holeMapZ - offsetZ, 0, terrainData.holesResolution - holeHeight);
-
-            bool[,] holes = terrainData.GetHoles(startX, startZ, holeWidth, holeHeight);
-
-            Vector2 center = new Vector2(holeWidth / 2f, holeHeight / 2f);
-            float radius = holeWidth / 2f;
-
-            for (int x = 0; x < holeWidth; x++)
-            {
-                for (int z = 0; z < holeHeight; z++)
-                {
-                    float dist = Vector2.Distance(new Vector2(x, z), center);
-                    holes[z, x] = dist > radius;
-                }
-            }
-
-            terrainData.SetHoles(startX, startZ, holes);
+            CreateHole(contactPoint);
+            CreateHoleClientRpc(contactPoint);
         }
+    }
+    [ClientRpc]
+    private void CreateHoleClientRpc(Vector3 contactPoint)
+    {
+        if (IsServer) return; 
+        CreateHole(contactPoint);
+    }
+    void CreateHole(Vector3 contactPoint)
+    {
+        TerrainData terrainData = t.terrainData;
+        Vector3 terrainPos = t.transform.position;
+
+        float relativeX = (contactPoint.x - terrainPos.x) / terrainData.size.x;
+        float relativeZ = (contactPoint.z - terrainPos.z) / terrainData.size.z;
+
+        int holeMapX = (int)(relativeX * terrainData.holesResolution);
+        int holeMapZ = (int)(relativeZ * terrainData.holesResolution);
+
+        int offsetX = holeWidth / 2;
+        int offsetZ = holeHeight / 2;
+
+        int startX = Mathf.Clamp(holeMapX - offsetX, 0, terrainData.holesResolution - holeWidth);
+        int startZ = Mathf.Clamp(holeMapZ - offsetZ, 0, terrainData.holesResolution - holeHeight);
+
+        bool[,] holes = terrainData.GetHoles(startX, startZ, holeWidth, holeHeight);
+
+        Vector2 center = new Vector2(holeWidth / 2f, holeHeight / 2f);
+        float radius = holeWidth / 2f;
+
+        for (int x = 0; x < holeWidth; x++)
+        {
+            for (int z = 0; z < holeHeight; z++)
+            {
+                float dist = Vector2.Distance(new Vector2(x, z), center);
+                holes[z, x] = dist > radius;
+            }
+        }
+
+        terrainData.SetHoles(startX, startZ, holes);
     }
 }
