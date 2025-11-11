997288af5add9a5a40ce71a85ae128dde8ec9aba
Maria Smirnova <92306135+Maria-Senua@users.noreply.github.com>
2025-11-11 16:35:58 +0100

    made dust network

--- Assets/NGO_Minimal_Setup/Scripts/DustMaker.cs
+++ Assets/NGO_Minimal_Setup/Scripts/DustMaker.cs
@@ -1,17 +1,25 @@
 using System;
 using UnityEngine;
+using Unity.Netcode;
 
-public class DustMaker : MonoBehaviour
+public class DustMaker : NetworkBehaviour
 {
     [SerializeField] private GameObject dust;
 
     private void OnCollisionEnter(Collision other)
     {
+        if (!IsServer) return;
         if (other.gameObject.tag == "Destructible")
         {
             var contactPoint = other.contacts[0].point;
-            var newDust = Instantiate(dust, contactPoint, Quaternion.identity);
-            Destroy(newDust, 10f);
+            SpawnDustClientRpc(contactPoint);
         }
+    }
+    
+    [ClientRpc]
+    private void SpawnDustClientRpc(Vector3 position)
+    {
+        var newDust = Instantiate(dust, position, Quaternion.identity);
+        Destroy(newDust, 10f);
     }
 }
